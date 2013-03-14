3c9b8328d243ea8f5fd84d43382d8354da794bef
Colin Rotherham <work@colinr.com>
2013-03-14 08:46:31 +0000

    Correct ARIA role attribute

--- js/slideshow.js
+++ js/slideshow.js
@@ -237,7 +237,7 @@
 			var length = self.slides.length;
 			while (length--)
 			{
-				markers.prepend($('<li><a href="#" aria-role="button">' + (length + 1) + '</a></li>'));
+				markers.prepend($('<li><a href="#" role="button">' + (length + 1) + '</a></li>'));
 			}
 
 			// Find the new links
