b5b8d68d576e7e2f52d0ca4ab3c0f4e87c518013
Colin Rotherham <work@colinr.com>
2015-03-26 21:14:24 +0000

    Set up basic Gulp build workflow
    Ready-made pre-built demo version now in /dist/

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+/*
+	Dependencies
+	----------------------------------- */
+
+	var gulp = require('gulp'),
+		sass = require('gulp-sass'),
+		uglify = require('gulp-uglify'),
+		prefix = require('gulp-autoprefixer'),
+		minifyCSS = require('gulp-minify-css'),
+		rename = require('gulp-rename'),
+		del = require('del');
+
+/*
+	Shared options
+	----------------------------------- */
+
+	var options = {
+
+		sass: {
+			style: 'compressed',
+			errLogToConsole: true
+		},
+
+		prefix: {
+			browsers: ['> 5%', 'IE >= 7', 'iOS >= 5.1'],
+			cascade: false,
+			remove: true
+		},
+
+		minifyCSS: {
+			keepSpecialComments: false
+		},
+	};
+
+
+/*
+	Clean output directory
+	----------------------------------- */
+
+	gulp.task('clean', function(callback) {
+		return del(['./dist/assets/js/*', './dist/assets/css/*'], callback);
+	});
+
+
+/*
+	Copy other assets (e.g. jQuery)
+	----------------------------------- */
+
+	gulp.task('copy', function(callback) {
+
+		gulp.src('./node_modules/jquery/dist/jquery.min.js')
+			.pipe(gulp.dest('./dist/assets/js'));
+	});
+
+
+/*
+	Sass to CSS
+	----------------------------------- */
+
+	gulp.task('sass', ['clean'], function() {
+
+		gulp.src('./src/scss/*.scss')
+			.pipe(sass(options.sass))
+			.pipe(prefix(options.prefix))
+			.pipe(minifyCSS(options.minifyCSS))
+			.pipe(rename({ suffix: '.min' }))
+			.pipe(gulp.dest('./dist/assets/css'));
+	});
+
+
+/*
+	Minify JS
+	----------------------------------- */
+
+	gulp.task('uglify', ['clean'], function() {
+
+		gulp.src('./src/js/slideshow.js')
+			.pipe(uglify())
+			.pipe(rename({ suffix: '.min' }))
+			.pipe(gulp.dest('./dist/assets/js'));
+	});
+
+
+/*
+	Define tasks
+	----------------------------------- */
+
+	// Default
+	gulp.task('default', ['copy', 'sass', 'uglify']);
